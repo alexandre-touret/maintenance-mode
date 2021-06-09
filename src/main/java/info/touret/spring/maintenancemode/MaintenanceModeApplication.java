fed6e83b76536da9cbcba2a8790a7f8a1d341c7b
Alexandre Touret <alexandre.touret@free.fr>
2021-06-09 16:32:28 +0200

    Initialization

---
+++ src/main/java/info/touret/spring/maintenancemode/MaintenanceModeApplication.java
@@ -0,0 +1,13 @@
+package info.touret.spring.maintenancemode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceModeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/touret/spring/maintenancemode/MaintenanceModeApplicationTests.java
@@ -0,0 +1,13 @@
+package info.touret.spring.maintenancemode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
